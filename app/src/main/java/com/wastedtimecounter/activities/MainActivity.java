2fd9b5bd04e01fca0218e508485963abf5d82875
Ven <thevenegance@bigmir.net>
2016-01-25 14:47:29 +0200

    Some changes in MainActivity

--- app/src/main/java/com/wastedtimecounter/activities/MainActivity.java
+++ app/src/main/java/com/wastedtimecounter/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -18,14 +19,22 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+
 import com.wastedtimecounter.adapters.ApplicationAdapter;
 import com.wastedtimecounter.R;
 import com.wastedtimecounter.fragments.FragmentPageAdapter;
 import com.wastedtimecounter.services.ApplicationsListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,32 +64,53 @@ public class MainActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Calendar calendar = Calendar.getInstance();
+                Resources res = getResources();
                 switch (position) {
                     case 0: {
-                        Calendar calendar = Calendar.getInstance();
+                        String args[]=new String[calendar.get(Calendar.DAY_OF_MONTH)];
+                        for (int i = calendar.get(Calendar.DAY_OF_MONTH); i >=1; i--) {
 
-                        String year = String.valueOf(calendar.get(Calendar.YEAR));
-                        String month = String.format("%02d", calendar.get(Calendar.MONTH) + 1);
-                        String day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+                            String month = formatMonth(Calendar.MONTH - 1, Locale.getDefault());
+                            String day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH) - i + 1);
 
-                        String date = day + "." + month + "." + year;
-                        String args[] = {date};
-                        viewPager.setAdapter(new FragmentPageAdapter(getSupportFragmentManager(),
-                                MainActivity.this, 1, args));
+                            String date = day + " " + month;
+                            args[i-1] = date;
+                        }
+                        Collections.reverse(Arrays.asList(args));
+                            viewPager.setAdapter(new FragmentPageAdapter(getSupportFragmentManager(),
+                                    MainActivity.this, calendar.get(Calendar.DAY_OF_MONTH), args));
+
                         tabLayout.setupWithViewPager(viewPager);
                         break;
                     }
                     case 1: {
-                        String args[] = {"Week 1", "Week 2"};
+                        String args[]=new String[calendar.get(Calendar.WEEK_OF_MONTH)];
+                        for (int i = calendar.get(Calendar.WEEK_OF_MONTH); i >=1; i--) {
+
+                            String wom = String.valueOf(calendar.get(Calendar.WEEK_OF_MONTH) - i + 1);
+
+                            String date = wom + " " + getString(R.string.week_of_month);
+                            args[i-1] = date;
+                        }
+                        Collections.reverse(Arrays.asList(args));
                         viewPager.setAdapter(new FragmentPageAdapter(getSupportFragmentManager(),
-                                MainActivity.this, 2, args));
+                                MainActivity.this, calendar.get(Calendar.WEEK_OF_MONTH), args));
+
                         tabLayout.setupWithViewPager(viewPager);
                         break;
                     }
                     case 2: {
-                        String args[] = {"Month 1", "Month 2", "Month 3"};
+                        String args[]=new String[calendar.get(Calendar.MONTH)+1];
+                        for (int i = calendar.get(Calendar.MONTH)+1; i >=1; i--) {
+
+                            String date = res.getStringArray(R.array.months)[Calendar.MONTH - 2];
+                            args[i-1] = date;
+                        }
+                        Collections.reverse(Arrays.asList(args));
                         viewPager.setAdapter(new FragmentPageAdapter(getSupportFragmentManager(),
-                                MainActivity.this, 3, args));
+                                MainActivity.this, calendar.get(Calendar.MONTH) + 1, args));
+
                         tabLayout.setupWithViewPager(viewPager);
                         break;
                     }
@@ -109,4 +139,15 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public String getMonth(int month) {
+        return new DateFormatSymbols().getMonths()[month-1];
+    }
+
+    public String formatMonth(int month, Locale locale) {
+        DateFormat formatter = new SimpleDateFormat("MMMM", locale);
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.MONTH, month-1);
+        return formatter.format(calendar.getTime());
+    }
 }
