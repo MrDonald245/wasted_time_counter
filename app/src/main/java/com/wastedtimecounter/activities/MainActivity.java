ed4e29504cbfbc5a525b0a1718379c9dc742b584
Eugene <ebocharnikov96@mail.ru>
2016-01-25 18:02:59 +0200

    Settings isn't working(

--- app/src/main/java/com/wastedtimecounter/activities/MainActivity.java
+++ app/src/main/java/com/wastedtimecounter/activities/MainActivity.java
@@ -1,28 +1,22 @@
 package com.wastedtimecounter.activities;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.wastedtimecounter.R;
-import com.wastedtimecounter.adapters.ApplicationAdapter;
-import com.wastedtimecounter.preferences.support.ThemeSupport;
-import com.wastedtimecounter.services.ApplicationsListener;
 import com.wastedtimecounter.fragments.FragmentPageAdapter;
+import com.wastedtimecounter.preferences.support.ThemeSupport;
 
 import java.text.DateFormat;
 import java.text.DateFormatSymbols;
@@ -55,55 +49,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
 
-    /**
-     * Menu handler.
-     *
-     * @param item View which call the handler.
-     * @return true if all is ok, false otherwise.
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                openPreferenceActivity();
-                return true;
-            default:
-                return false;
-        }
-    }
 
-
-    /**
-     * Show preference activity.
-     */
-    private void openPreferenceActivity() {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
-
-
-    private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
-        ArrayList<ApplicationInfo> applist = new ArrayList<>();
-        for (ApplicationInfo info : list) {
-            try {
-                if (null != packageManager.getLaunchIntentForPackage(info.packageName))
-                    applist.add(info);
-            } catch (Exception e) {
-                e.printStackTrace();
         this.arr = new String[]{"Day", "Week", "Month"};
         Spinner spinner = (Spinner) findViewById(R.id.spinner_nav);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -165,10 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
-        }
-        return applist;
-    }
-
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -186,10 +131,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
+    /**
+     * Show preference activity.
+     */
+    private void openPreferenceActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Menu handler.
+     *
+     * @param item View which call the handler.
+     * @return true if all is ok, false otherwise.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                openPreferenceActivity();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
 
     public String getMonth(int month) {
         return new DateFormatSymbols().getMonths()[month - 1];
